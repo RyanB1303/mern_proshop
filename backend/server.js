1bff9bd3c190375915f675099e4592e0e42dad26
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-10-05 01:29:28 +0700

    backend complete with local data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
-const port = 5000;
-const products = require("./data/products");
+const port = process.env.PORT || 5000;
+import products from "./data/products.js";
 
-app.get("/", (req, res) => res.send("API HERE"));
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API Page , please use it wisely");
+});
 
 app.get("/api/products", (req, res) => {
   res.json(products);
@@ -13,4 +18,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(port, () => console.log(`Server is Running on port `));
+app.listen(port, () =>
+  console.log(`Server is Running in ${process.env.NODE_ENV} on port ${port}`)
+);
