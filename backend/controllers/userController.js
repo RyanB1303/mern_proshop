2f63c37d99c683e518170b402bb268e6754fc9cf
Ryan B <ryan.brilliant.nirwana@gmail.com>
2020-10-10 11:10:37 +0700

    fix middleware and add register route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,40 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    REGISTER USER
+// @route   POST /api/users
+// @access  public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existingUserChecker = await User.findOne({ email });
+
+  if (existingUserChecker) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const createUser = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (createUser) {
+    res.status(201).json({
+      _id: createUser._id,
+      name: createUser.name,
+      email: createUser.email,
+      isAdmin: createUser.isAdmin,
+      token: generateJWT(createUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Cant Retrieve Data , User Not Found");
+  }
+});
+
 // @desc    GET USER PROFILE
 // @route   GET /api/users/profile
 // @access  private (protected)
@@ -44,4 +78,10 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, getAllUsers };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  getAllUsers,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", authUser);
+router.route("/").post(registerUser);
 router.route("/profile").get(protect, getUserProfile);
+router.route("/all").get(getAllUsers);
 
 export default router;
