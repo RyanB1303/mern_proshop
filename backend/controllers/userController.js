6a773e2d180543968aff50d40945c3f8b09403b2
ryanB1771 <ryan.brilliant.nirwana@gmail.com>
2020-11-12 20:50:22 +0700

    update profile functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,10 +78,43 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    UPDATE USER PROFILE
+// @route   PUT /api/users/profile
+// @access  private (protected)
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    // save the updated profile to variable name updatedUser
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateJWT(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
 
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, getAllUsers };
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getAllUsers,
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,6 +4,7 @@ import {
   getUserProfile,
   registerUser,
   getAllUsers,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -12,6 +13,7 @@ const router = express.Router();
 router.post("/login", authUser);
 router.route("/").post(registerUser);
 router.route("/profile").get(protect, getUserProfile);
+router.route("/profile").put(protect, updateUserProfile);
 router.route("/all").get(getAllUsers);
 
 export default router;
